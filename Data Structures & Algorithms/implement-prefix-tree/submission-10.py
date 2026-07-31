b67fb7cb80eb8fcbe5eb60368797c2fa4cf6a982
Isaac Lee <101655182+ssIsaac@users.noreply.github.com>
2026-07-31 22:07:34 +0800

    Add: implement-prefix-tree - submission-10

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-10.py
@@ -0,0 +1,41 @@
+class PrefixTree:
+
+    class Node:
+        def __init__(self,next=None, isEnd=False):
+            self.next = {}
+            self.isEnd = isEnd
+        
+
+    def __init__(self):
+        self.root = self.Node()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for w in word:
+            ## new node
+            if w not in curr.next:
+                curr.next[w] = self.Node(w) 
+            curr = curr.next[w]
+        curr.isEnd = True
+        
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for w in word:
+            if w not in curr.next:
+                return False
+            curr = curr.next[w]
+            
+        return curr.isEnd
+
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for w in prefix:
+            if w not in curr.next:
+                return False
+            curr = curr.next[w]      
+        return True
+        
+        
